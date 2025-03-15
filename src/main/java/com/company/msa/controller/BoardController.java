4399df4ced8c16a160dd8acc932bee8586625fe9
hamjinoo <hjw3012@naver.com>
2025-03-15 11:35:14 +0900

    Hello, Board API 구현

---
+++ src/main/java/com/company/msa/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.company.msa.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+public class BoardController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Board API!";
+    }
+}
